30ca6afab272e238526289c0480094f62cff8768
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-14 18:42:08 -0300

    Add files via upload

---
+++ ByteBank.Modelos/AutenticacaoHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    internal class AutenticacaoHelper
+    {
+        public bool CompararaSenhas(string senhaVerdadeira, string senhaTentativa)
+        {
+            return senhaTentativa == senhaVerdadeira;
+        }
+    }
+}
---
+++ ByteBank.Modelos/ParceiroComercial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    public class ParceiroComercial : IAutenticavel
+    {
+        public string Senha { get; set; }
+        private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
+
+        public bool Autenticar(string senha)
+        {
+            return _autenticacaoHelper.CompararaSenhas(Senha, senha);
+        }
+    }
+}
