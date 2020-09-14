f85377abbee2324dc6a259fdf6b1d29b6089a7ad
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-09-14 18:42:28 -0300

    Add files via upload

---
+++ ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.Modelos.Funcionarios
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        public string Senha { get; set; }
+        private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
+
+        public FuncionarioAutenticavel(double salario, string cpf)
+            : base(salario, cpf)
+        {
+        }
+
+        public bool Autenticar(string senha)
+        {
+            return _autenticacaoHelper.CompararaSenhas(Senha, senha);
+        }
+    }
+}
